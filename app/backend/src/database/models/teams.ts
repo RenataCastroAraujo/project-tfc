95be5b8a01e18d754df446ebd8f261adb8b66a52
RenatadeCastroAraujo <renata_castroaraujo@yahoo.com.br>
2022-09-28 14:09:34 -0300

    feat: :construction: alteração de requisito 01 e inclusão requisito 14 e 18

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,27 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class TeamsModel extends Model {
+  id?: number;
+  teamName: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  tableName: 'teams',
+});
+
+export default TeamsModel;
---
+++ app/backend/src/interfaces/IUser.ts
@@ -0,0 +1,7 @@
+export default interface IUser {
+  id?: number;
+  username: string;
+  role: string;
+  password: string;
+  email: string;
+}
